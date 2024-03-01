6f35b2432321edc06b23ef8099014b68e699c0e6
ChakibA11 <chakchak@Chaks-MacBook-Pro.local>
2024-02-29 18:02:09 -0600

    commit 2

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,18 @@
+import Nav from '../components/Nav'
+import { useState } from 'react'
+
+import { useNavigate } from 'react-router-dom'
+
+
+const Dashboard = () => {
+
+
+    return (
+
+        <div>Dashboard</div>
+
+
+    )
+}
+
+export default Dashboard;
